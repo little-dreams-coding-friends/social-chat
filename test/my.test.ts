57ca6ab522bc9412bfab2f21608b4276c8dd8ac9
ProwlingLynx <tamezjavier@gmail.com>
2022-04-14 18:46:30 -0500

    Removing babel from tool chain. Adding configs for jest and modifying webpack to work with TSC instead of babel. Adding a simple test to prove config functionality in my.test.ts

---
+++ test/my.test.ts
@@ -0,0 +1,7 @@
+const x = 1;
+let y: number = 3;
+
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(x + y).toBe(3);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,17 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-const isProduction = process.env.NODE_ENV == "production";
-
-const stylesHandler = MiniCssExtractPlugin.loader;
-
-const config = {
+module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    path: __dirname + "/dist"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-
-    new MiniCssExtractPlugin(),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components|server)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "@babel/preset-env",
-              {
-                "useBuiltIns": "entry",
-                "modules": false
-              },
-              "@babel/preset-react",
-              "@babel/plugin-transform-typescript",
-              {
-                "development": true
-              },
-              "@babel/preset-typescript",
-              {
-                "allExtensions": true
-              }
-            ]
-          }
-      }
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
+  //devtool: "source-map"
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
+  module: {
+    rules: [{
+      test: /].tsx?$/,
+      loader: "ts-loader"
+    }]
   }
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
-};
+}
